13610d290432159f5cbd615be18810047499525f
esraarabee1 <esraarabee69@gmail.com>
2025-04-14 15:58:08 -0700

    logic and design for menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,52 +1,41 @@
 import React, { useState } from "react";
-import {
-  Navbar,
-  Container,
-  Row,
-  Nav,
-  Form,
-  Button,
-  FormControl,
-  NavDropdown,
-} from "react-bootstrap";
+import { Navbar, Container, Nav, Form, FormControl } from "react-bootstrap";
 
 const NavBar = ({ filterbySearch }) => {
   const [searchValue, setSearchValue] = useState("");
+
   const onSearch = (e) => {
     e.preventDefault();
     filterbySearch(searchValue);
     setSearchValue("");
   };
+
   return (
-    <Row>
-      <Navbar bg="dark" expand="lg" variant="dark">
-        <Container>
-          <Navbar.Brand href="#">
-            <div className="brand-color">مطعم جديد</div>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            ></Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="text"
-                placeholder="ابحث.."
-                className="mx-2"
-                onChange={(e) => setSearchValue(e.target.value)}
-                value={searchValue}
-              />
-              <button onClick={onSearch} className="btn-search">
-                بحث
-              </button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Row>
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#">
+          <div className="brand-color">مطعم على نار هادية</div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          ></Nav>
+          <Form className="d-flex" onSubmit={onSearch}>
+            <FormControl
+              type="text"
+              placeholder="ابحث عن طبقك المفضل"
+              className="mx-2"
+              onChange={(e) => setSearchValue(e.target.value)}
+              value={searchValue}
+            />
+            <button className="btn-search">بحث</button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
